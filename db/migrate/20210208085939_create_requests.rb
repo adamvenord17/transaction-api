baca014f6bf761808f9cc5039670e89e10929083
supermavens <sunavenue.ca@gmail.com>
2021-02-08 01:01:35 -0800

    first commit

---
+++ db/migrate/20210208085939_create_requests.rb
@@ -0,0 +1,21 @@
+class CreateRequests < ActiveRecord::Migration[6.1]
+  def change
+    create_table :requests do |t|
+      t.string :bk_organization_id
+      t.string :source_uuid
+      t.integer :transactions_summerized_count
+      t.float :summarized_net_sales
+      t.string :journal_entry_template
+      t.text :json_diff
+      t.string :connection_id
+      t.string :short_summary
+      t.string :source_raw_data
+      t.string :apify_run_url
+      t.integer :bk_external_id
+      t.jsonb :post_raw_data
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
